f1e14c87056e6c6dd18c028afa59b899b8e52293
Adam Kobi <adamkobi12@gmail.com>
2020-03-28 23:10:12 +0300

    change more import paths

--- _example/specific/specific.go
+++ _example/specific/specific.go
@@ -7,9 +7,9 @@ import (
 	"github.com/apex/log"
 	"github.com/kierdavis/ansi"
 
-	"github.com/tj/go-update"
-	"github.com/tj/go-update/progress"
-	"github.com/tj/go-update/stores/github"
+	"github.com/adamkobi/go-update"
+	"github.com/adamkobi/go-update/progress"
+	"github.com/adamkobi/go-update/stores/github"
 )
 
 func init() {
